34586ae0ab337010839a468ae2c5bff3ca38547a
Square <aayushk4980@gmail.com>
2024-07-24 12:43:03 +0530

    Added my image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import BackgroundAnimation from './components/background/BackgroundAnimation';
+import myImage from './images/myImg.jpeg';
+import BackgroundAnimation from './components/background/BackgroundAnimation.jsx';
 
 export default function App() {
 
   return (
     <>
       <BackgroundAnimation />
+
+      <a
+        href="https://github.com/Aayush259"
+        target="_blank"
+        className="fixed z-20 right-4 top-4 block duration-300 hover:scale-110"
+      >
+        <img
+          src={myImage}
+          alt="Aayush"
+          className="rounded-full h-10 md:h-14"
+        />
+      </a>
     </>
   );
 };
