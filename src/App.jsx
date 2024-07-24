43b8d54b11aefa0fb9e7053dfe082e7f39b061ae
Square <aayushk4980@gmail.com>
2024-07-24 12:34:09 +0530

    Initialized project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+
+  return (
+    <>
+      <p className="bg-black text-white border">
+        Animated Portfolio
+      </p>
+    </>
+  );
+};
