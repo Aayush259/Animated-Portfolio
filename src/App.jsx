4a15dfb934abfc09d79e4da4b18f4589ecdc6032
Square <aayushk4980@gmail.com>
2024-07-25 13:31:46 +0530

    Reduced starting welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   useEffect(() => {
     const introTimer = setTimeout(() => {
       setShowIntro(false);
-    }, 4000);
+    }, 3900);
 
     // Cleanup the timer.
     return () => clearTimeout(introTimer);
