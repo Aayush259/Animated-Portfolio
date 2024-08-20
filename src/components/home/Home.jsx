4e6daf6ba5689b97e090bac8c28918af49523dbd
Square <aayushk4980@gmail.com>
2024-08-21 01:10:57 +0530

    Home Page color changed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,23 +6,23 @@ export default function Home() {
 
     return (
         <div
-            className="bg-home-bg border-4 p-5 m-4 z-10 border-home-bg rounded-3xl relative md:max-w-[750px] lg:left-20 md:m-5 duration-300"
+            className="bg-home-bg border-4 p-5 m-4 z-40 border-home-bg rounded-3xl relative md:max-w-[750px] lg:left-20 md:m-5 duration-300"
         >
 
             <p
-                className="font-semibold text-gray-600 text-lg md:text-3xl md:font-bold"
+                className="font-semibold text-white text-lg md:text-3xl md:font-bold"
             >
                 Hi! I'm
             </p>
 
             <p
-                className="font-bold leading-[2.8rem] text-purple-900 text-4xl my-2 md:text-7xl md:leading-[5.4rem] md:text-purple-950 md:font-extrabold"
+                className="font-bold leading-[2.8rem] bg-gradient-to-r from-[rgb(0,17,82)] to-[rgb(108,0,162)] bg-clip-text text-transparent text-4xl my-2 md:text-7xl md:leading-[5.4rem] md:font-extrabold"
             >
                 Aayush Kumar Kumawat
             </p>
 
             <p
-                className="font-semibold my-6 text-sm text-gray-600 flex flex-row flex-wrap gap-1 md:gap-2 md:text-2xl md:my-8"
+                className="font-semibold my-6 text-sm text-white flex flex-row flex-wrap gap-1 md:gap-2 md:text-2xl md:my-8"
             >
                 <span>and I</span>
 
