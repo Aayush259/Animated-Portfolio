c2330d03b4e0b2e0df872c74c00661737dd0f531
Square <aayushk4980@gmail.com>
2024-08-21 01:49:48 +0530

    Added nav animation using framer motion

--- src/components/nav/NavigationLink.jsx
+++ src/components/nav/NavigationLink.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function NavigationLink({ linkDetails }) {
+export default function NavigationLink({ linkDetails, index }) {
 
     // Getting details from linkDetails prop.
     const linkToUrl = linkDetails['linkToUrl'];
@@ -38,14 +39,24 @@ export default function NavigationLink({ linkDetails }) {
     }, []);
 
     return (
-        <NavLink
-            to={linkToUrl}
-            className={({isActive}) => `${isActive ? "before:bg-slate-950 lg:before:bg-green-400 lg:before:top-0 before:left-0 lg:before:h-full before:w-full before:h-1 lg:hover:opacity-60 text-slate-950 lg:text-green-400" : "before:bg-white lg:before:top-1/2 lg:before:h-0 before:w-0 before:left-1/2 before:h-1 lg:hover:before:h-full lg:hover:before:top-0"} flex flex-row items-center justify-center gap-2 text-lg text-orange-100 font-semibold tracking-wider cursor-pointer select-none duration-300 relative lg:before:block before:-bottom-2 before:absolute lg:before:-left-3 lg:before:w-1 before:rounded-full lg:before:opacity-100 before:duration-300`}
+        <motion.div
+            initial={{ x: `${-90 + (index * 30)}%` }}
+            animate={{ x: 0 }}
+            transition={{
+                duration: 0.3,
+                ease: 'easeInOut'
+            }}
+            className="overflow-hidden"
         >
-            <FontAwesomeIcon icon={linkIcon} />
-            {
-                linkContentText ? linkText : null
-            }
-        </NavLink>
+            <NavLink
+                to={linkToUrl}
+                className={({ isActive }) => `${isActive ? "before:bg-slate-950 lg:before:bg-green-400 lg:before:top-0 before:left-0 lg:before:h-full before:w-full before:h-1 lg:hover:opacity-60 text-slate-950 lg:text-green-400" : "before:bg-white lg:before:top-1/2 lg:before:h-0 before:w-0 before:left-1/2 before:h-1 lg:hover:before:h-full lg:hover:before:top-0"} flex flex-row items-center justify-center gap-2 text-lg text-orange-100 font-semibold tracking-wider cursor-pointer select-none duration-300 relative lg:before:block before:-bottom-2 before:absolute lg:before:-left-3 lg:before:w-1 before:rounded-full lg:before:opacity-100 before:duration-300`}
+            >
+                <FontAwesomeIcon icon={linkIcon} />
+                {
+                    linkContentText ? linkText : null
+                }
+            </NavLink>
+        </motion.div>
     );
 };
